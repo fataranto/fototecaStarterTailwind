5f6749c19495c8aec56e2d88194afe7a0754c067
fataranto <fataranto@gmail.com>
2022-06-17 12:13:52 +0200

    form and push object to array working

---
+++ README.md
@@ -0,0 +1,3 @@
+# Utils / Resources
+[Random pictures](https://www.pexels.com/es-es/)
+[Tailwind Forms Plugin](https://github.com/tailwindlabs/tailwindcss-forms)
--- app.js
+++ app.js
@@ -3,9 +3,64 @@ const path = require('path');
 
 const pictures = [ //luego lo sustituiré por un json para conservar el contenido
     {
-        title: "foto1",
-        URL: "url",
-        fecha: "fecha"
+        title: "Cactus",
+        URL: "https://randomwordgenerator.com/img/picture-generator/50e6d1414c50b10ff3d8992cc12c30771037dbf85254784e77267ed09444_640.jpg",
+        date: "10/01/22"
+    },
+    {
+        title: "Windows",
+        URL: "https://randomwordgenerator.com/img/picture-generator/53e3d54a4c57a814f1dc8460962e33791c3ad6e04e507440762e7ad39344c4_640.jpg",
+        date: "18/11/20"
+    },
+    {
+        title: "Buildings",
+        URL: "https://randomwordgenerator.com/img/picture-generator/53e1d7444d53af14f1dc8460962e33791c3ad6e04e507440742a7ad19f4cc0_640.jpg",
+        date: "25/09/18"
+    },
+    {
+        title: "Hidden kitty",
+        URL: "https://randomwordgenerator.com/img/picture-generator/57e3d3444a52ac14f1dc8460962e33791c3ad6e04e50744172297cdd9f45c7_640.jpg",
+        date: "12/03/21"
+    },
+    {
+        title: "Cactus",
+        URL: "https://randomwordgenerator.com/img/picture-generator/50e6d1414c50b10ff3d8992cc12c30771037dbf85254784e77267ed09444_640.jpg",
+        date: "10/01/22"
+    },
+    {
+        title: "Windows",
+        URL: "https://randomwordgenerator.com/img/picture-generator/53e3d54a4c57a814f1dc8460962e33791c3ad6e04e507440762e7ad39344c4_640.jpg",
+        date: "18/11/20"
+    },
+    {
+        title: "Buildings",
+        URL: "https://randomwordgenerator.com/img/picture-generator/53e1d7444d53af14f1dc8460962e33791c3ad6e04e507440742a7ad19f4cc0_640.jpg",
+        date: "25/09/18"
+    },
+    {
+        title: "Hidden kitty",
+        URL: "https://randomwordgenerator.com/img/picture-generator/57e3d3444a52ac14f1dc8460962e33791c3ad6e04e50744172297cdd9f45c7_640.jpg",
+        date: "12/03/21"
+    },
+    {
+        title: "Cactus",
+        URL: "https://randomwordgenerator.com/img/picture-generator/50e6d1414c50b10ff3d8992cc12c30771037dbf85254784e77267ed09444_640.jpg",
+        date: "10/01/22"
+    },
+    {
+        title: "Windows",
+        URL: "https://randomwordgenerator.com/img/picture-generator/53e3d54a4c57a814f1dc8460962e33791c3ad6e04e507440762e7ad39344c4_640.jpg",
+        date: "18/11/20"
+    },
+    {
+        title: "Buildings",
+        URL: "https://randomwordgenerator.com/img/picture-generator/53e1d7444d53af14f1dc8460962e33791c3ad6e04e507440742a7ad19f4cc0_640.jpg",
+        date: "25/09/18"
+    },
+    {
+        title: "Hidden kitty",
+        URL: "https://randomwordgenerator.com/img/picture-generator/57e3d3444a52ac14f1dc8460962e33791c3ad6e04e50744172297cdd9f45c7_640.jpg",
+        date: "12/03/21"
     }
 ];
 
@@ -16,10 +71,45 @@ app.set('view engine', 'ejs');
 //app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static('public'))
 
+app.use(express.urlencoded({
+    extended: true
+})); //para obtner datos del formulario via POST
+
 app.get("/", (req, res) => {
     res.render("index", {
-        numPics: pictures.length
+        numPics: pictures.length,
+        pics: pictures
     });
+})
+
+
+
+app.get("/upload", (req, res) => {
+    res.render("form");
+})
+
+app.post('/imguploaded', function (req, res) {
+
+    let myNewPic = {
+        title: req.body.title,
+        URL: req.body.url,
+        date: req.body.date
+    }
+
+    pictures.push(myNewPic);
+
+    //console.log(pictures);
+
+    res.render("index", {
+        numPics: pictures.length,
+        pics: pictures
+    });
+
+})
+
+
+app.use((req, res) => {
+    res.status(404).render('404');
 })
 
 const server = app.listen(3000, () => {
