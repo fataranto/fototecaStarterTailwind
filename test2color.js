583857528d2522497072bbf203175784b04c651c
fataranto <fataranto@gmail.com>
2022-06-20 19:56:47 +0200

    color-thief working needs implementation

--- test2color.js
+++ test2color.js
@@ -1,10 +1,25 @@
-const ColorThief = require('colorthief');
-const img = resolve(process.cwd(), 'cat.jpeg');
+const { getColorFromURL } = require('color-thief-node');
+const { getPaletteFromURL } = require('color-thief-node');
 
-ColorThief.getColor(img)
-    .then(color => { console.log(color) })
-    .catch(err => { console.log(err) })
+const imageURL = "https://randomwordgenerator.com/img/picture-generator/50e6d1414c50b10ff3d8992cc12c30771037dbf85254784e77267ed09444_640.jpg";
 
-ColorThief.getPalette(img, 5)
-    .then(palette => { console.log(palette) })
-    .catch(err => { console.log(err) })
+
+
+
+  const myColor =  (async (imageURL) => {
+        const dominantColor = await getColorFromURL(imageURL);
+        console.log(dominantColor);
+    });
+
+
+    const myColorPallete = (async (imageURL) => {
+        const colorPallete = await getPaletteFromURL(imageURL);
+        console.log(colorPallete);
+    });
+
+    //myColor(imageURL);
+    myColorPallete(imageURL);
+
+
+
+
